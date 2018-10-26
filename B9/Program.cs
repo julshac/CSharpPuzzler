3c1c572f5e69bf2b7b373311abfc5c5e9aa13848
Julia Shats <julia7476@gmail.com>
2018-10-27 01:57:46 +0300

    Initial commit
    Modified puzzler

--- B9/Program.cs
+++ B9/Program.cs
@@ -1,11 +1,19 @@
 using System;
-
 namespace PuzzlerB9
 {
-  static void Main(string[] args)
+  class Program
+  {
+    static void Foo(out int x, out int y)
     {
       x = 42;
       y = 123;
       Console.WriteLine (x == y);
+    }
+  
+     static void Main(string[] args)
+    {
+      int parameter = Convert.ToInt32(Console.ReadLine());
+      Foo(out parameter, out parameter)
+      Foo(parameter, parameter)
     }
 }
